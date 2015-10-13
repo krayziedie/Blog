a5ebf4da5d238615b6cdd710f0d713b63ca88ad8
krayziedie <diego.aguileram@gmail.com>
2015-10-13 17:11:28 -0300

    models validations added

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,3 +1,5 @@
 class Comentario < ActiveRecord::Base
   belongs_to :post
+  validates :contenido , presence: true, confirmation: true
+  validates :contenido_confirmation, presence: { message: " es requerido"}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comentarios
+	validates :content , presence: true , confirmation: true
+    validates :content_confirmation, presence: { message: " es requerido"}
 end
